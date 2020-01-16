3f6d2a637d85adbcca744d0fa75b29b7c909c3aa
Gareth James <gareth.james@pureclarity.com>
2020-01-16 16:38:38 +0000

    Improved Signup Status & Submit to separate validation, requesting & processing into seperate classes.

--- src/PureClarity/Api/Signup/Status.php
+++ src/PureClarity/Api/Signup/Status.php
@@ -7,27 +7,25 @@
 namespace PureClarity\Api\Signup;
 
 use Exception;
-use PureClarity\Api\Resource\Endpoints;
-use PureClarity\Api\Resource\Regions;
-use PureClarity\Api\Transfer\Curl;
+use PureClarity\Api\Signup\Status\Validator;
+use PureClarity\Api\Signup\Status\Requestor;
+use PureClarity\Api\Signup\Status\Processor;
 
 /**
  * Class Status
+ *
+ * Handles Signup Status requests - used to verify if a signup is complete.
  */
 class Status
 {
-    /**
-     * Required parameters for this request
-     *
-     * @var string[]
-     */
-    private $requiredParams = [
-        'id' => 'Request ID',
-        'region' => 'Region',
-    ];
+    /** @var string */
+    const PARAM_ID = 'id';
+
+    /** @var string */
+    const PARAM_REGION = 'region';
 
     /**
-     * Sends the signup request to PureClarity
+     * Validates, Sends & Processes Signup Status requests
      *
      * @param mixed[] $params
      *
@@ -35,119 +33,46 @@ class Status
      */
     public function request($params)
     {
-        $result = [
-            'error' => '',
-            'status' => '',
-            'response' => '',
-            'complete' => false,
-        ];
+        $validator = new Validator();
 
-        $errors = $this->validate($params);
-
-        if (empty($errors)) {
-            try {
-                $response = $this->sendRequest($params['id'], $params['region']);
-
-                $result['status'] = $response['status'];
-                $result['response'] = $response['body'] ? (array)json_decode($response['body']) : '';
-
-                if ($response['status'] === 400) {
-                    $responseData = json_decode($response['body']);
-                    $result['error'] = 'Signup status error: ' . implode('|', $responseData['errors']);
-                } elseif ($response['status'] !== 200) {
-                    $result['error'] = 'PureClarity server error occurred. If this persists, '
-                                     . 'please contact PureClarity support. Error code ' . $response['status'];
-                } else {
-                    if ($result['response']['Complete'] === true) {
-                        $result['response'] = [
-                            'access_key' => $result['response']['AccessKey'],
-                            'secret_key' => $result['response']['SecretKey'],
-                            'region' => $params['region']
-                        ];
-
-                        $result['complete'] = true;
-                    }
-                }
-            } catch (Exception $e) {
-                if (strpos($e->getMessage(), 'timed') !== false) {
-                    $result['error'] = 'Connection to PureClarity server timed out, please try again';
-                } else {
-                    $result['error'] = 'A general error occurred: ' . $e->getMessage();
-                }
-            }
+        if ($validator->isValid($params)) {
+            $result = $this->handleRequest($params);
         } else {
-            $result['error'] = implode(',', $errors);
+            $result = [
+                'status' => 0,
+                'errors' => $validator->getErrors(),
+                'response' => [],
+                'complete' => false,
+            ];
         }
 
         return $result;
     }
 
     /**
-     * @param $requestId
-     * @param $region
-     * @return array
-     * @throws Exception
-     */
-    private function sendRequest($requestId, $region)
-    {
-        $request = $this->buildRequest($requestId);
-        $url = $this->getSignupStatusApiEndpointUrl($region);
-
-        $curl = new Curl();
-
-        $curl->post($url, $request);
-
-        return [
-            'status' => $curl->getStatus(),
-            'body' => $curl->getBody()
-        ];
-    }
-
-    /**
-     * @param $region
-     * @return string
-     * @throws Exception
-     */
-    private function getSignupStatusApiEndpointUrl($region)
-    {
-        return Endpoints::getSignupStatusEndpoint($region);
-    }
-
-    /**
-     * Validates that all the necessary params are present and well formatted
-     *
+     * Requests & Processes the Signup Status request
      * @param mixed[] $params
-     * @return string[]
+     * @return mixed[]
      */
-    private function validate($params)
+    private function handleRequest($params)
     {
-        $errors = [];
-        // Check all required params are present
-        foreach ($this->requiredParams as $key => $label) {
-            if (!isset($params[$key]) || empty($params[$key])) {
-                $errors[] = 'Missing ' . $label;
-            }
-        }
-
-        if (Regions::isValidRegion($params['region']) === false) {
-            $errors[] = 'Invalid Region';
-        }
-
-        return $errors;
-    }
-
-    /**
-     * Builds the JSON for the request from the parameters provided
-     *
-     * @param string $requestId
-     * @return string
-     */
-    private function buildRequest($requestId)
-    {
-        $requestData = [
-            'Id' => $requestId
+        $result = [
+            'status' => 0,
+            'errors' => [],
+            'response' => [],
+            'complete' => false,
         ];
 
-        return json_encode($requestData);
+        try {
+            $requestor = new Requestor();
+            $response = $requestor->send($params['id'], $params['region']);
+
+            $processor = new Processor();
+            $result = $processor->process($response);
+        } catch (Exception $e) {
+            $result['errors'][] = 'Exception: ' . $e->getMessage();
+        }
+
+        return $result;
     }
 }
--- src/PureClarity/Api/Signup/Submit.php
+++ src/PureClarity/Api/Signup/Submit.php
@@ -6,37 +6,78 @@
 
 namespace PureClarity\Api\Signup;
 
-use PureClarity\Api\Resource\Endpoints;
-use PureClarity\Api\Resource\Regions;
-use PureClarity\Api\Transfer\Curl;
+use PureClarity\Api\Signup\Submit\Validator;
+use PureClarity\Api\Signup\Submit\Requestor;
+use PureClarity\Api\Signup\Submit\Processor;
 use Exception;
 
 /**
  * Class Submit
+ *
+ * Handles Signup requests - request to create a PureClarity acccount
  */
 class Submit
 {
-    /**
-     * Required parameters for this request
-     *
-     * @var string[]
-     */
-    private $requiredParams = [
-        'firstname' => 'First name',
-        'lastname' => 'Last name',
-        'email' => 'Email Address',
-        'company' => 'Company',
-        'password' => 'Password',
-        'store_name' => 'Store Name',
-        'region' => 'Region',
-        'url' => 'URL',
-        'platform' => 'Platform',
-        'currency' => 'Currency',
-        'timezone' => 'Timezone'
-    ];
+    /** @var string */
+    const PARAM_FIRSTNAME = 'firstname';
+
+    /** @var string */
+    const PARAM_LASTNAME = 'lastname';
+
+    /** @var string */
+    const PARAM_EMAIL = 'email';
+
+    /** @var string */
+    const PARAM_COMPANY = 'company';
+
+    /** @var string */
+    const PARAM_PASSWORD = 'password';
+
+    /** @var string */
+    const PARAM_STORE_NAME = 'store_name';
+
+    /** @var string */
+    const PARAM_REGION = 'region';
+
+    /** @var string */
+    const PARAM_URL = 'url';
+
+    /** @var string */
+    const PARAM_PLATFORM = 'platform';
+
+    /** @var string */
+    const PARAM_CURRENCY = 'currency';
+
+    /** @var string */
+    const PARAM_TIMEZONE = 'timezone';
+
+    /** @var string */
+    private $requestId;
 
     /**
-     * Sends the signup request to PureClarity
+     * Sets the Request ID for this request
+     * @param string $value
+     */
+    public function setRequestId($value)
+    {
+        $this->requestId = $value;
+    }
+
+    /**
+     * Gets the Request ID, generates one if not set
+     *
+     * @return string
+     */
+    public function getRequestId()
+    {
+        if ($this->requestId === null) {
+            $this->requestId = uniqid('', true);
+        }
+        return $this->requestId;
+    }
+
+    /**
+     * Validates & Sends the signup request to PureClarity
      *
      * @param mixed[] $params
      *
@@ -44,165 +85,49 @@ class Submit
      */
     public function request($params)
     {
-        $result = [
-            'error' => '',
-            'request_id' => '',
-            'status' => '',
-            'response' => '',
-            'success' => false,
-        ];
+        $validator = new Validator();
 
-        $errors = $this->validate($params);
-
-        if (empty($errors)) {
-            try {
-                $result['request_id'] = uniqid('', true);
-                $response = $this->sendRequest($result['request_id'], $params);
-
-                $result['status'] = $response['status'];
-                $result['response'] = $response['body'];
-
-                if ($response['status'] === 400) {
-                    $responseData = json_decode($response['body']);
-                    $result['error'] = 'Signup error: ' . implode('|', $responseData['errors']);
-                } elseif ($response['status'] !== 200) {
-                    $result['error'] = 'PureClarity server error occurred. If this persists, '
-                                     . 'please contact PureClarity support. Error code ' . $response['status'];
-                } else {
-                    $result['success'] = true;
-                }
-            } catch (Exception $e) {
-                if (strpos($e->getMessage(), 'timed') !== false) {
-                    $result['error'] = 'Connection to PureClarity server timed out, please try again';
-                } else {
-                    $result['error'] = 'A general error occurred: ' . $e->getMessage();
-                }
-            }
+        if ($validator->isValid($params)) {
+            $result = $this->handleRequest($params);
         } else {
-            $result['error'] = implode(',', $errors);
+            $result = [
+                'status' => 0,
+                'response' => [],
+                'errors' => $validator->getErrors(),
+                'success' => false,
+                'request_id' => ''
+            ];
         }
 
         return $result;
     }
 
     /**
-     * @param $requestId
-     * @param $params
-     * @return array
-     * @throws Exception
-     */
-    private function sendRequest($requestId, $params)
-    {
-        $request = $this->buildRequest($requestId, $params);
-        $url = $this->getSignupApiEndpointUrl($params['region']);
-
-        $curl = new Curl();
-
-        $curl->post($url, $request);
-
-        return [
-            'status' => $curl->getStatus(),
-            'body' => $curl->getBody()
-        ];
-    }
-
-    /**
-     * @param $region
-     * @return string
-     * @throws Exception
-     */
-    private function getSignupApiEndpointUrl($region)
-    {
-        return Endpoints::getSignupRequestEndpoint($region);
-    }
-
-    /**
-     * Validates that all the necessary params are present and well formatted
-     *
+     * Sends the Signup request & Processes the response
      * @param mixed[] $params
-     * @return string[]
+     * @return array|mixed[]
      */
-    private function validate($params)
+    private function handleRequest($params)
     {
-        $errors = [];
-        // Check all required params are present
-        foreach ($this->requiredParams as $key => $label) {
-            if (!isset($params[$key]) || empty($params[$key])) {
-                $errors[] = 'Missing ' . $label;
-            }
-        }
-
-        if (isset($params['email']) && !filter_var($params['email'], FILTER_VALIDATE_EMAIL)) {
-            $errors[] = 'Invalid Email Address';
-        }
-
-        if (isset($params['url']) && !$this->isValidUrl($params['url'], ['http', 'https'])) {
-            $errors[] = 'Invalid URL';
-        }
-
-        if (Regions::isValidRegion($params['region']) === false) {
-            $errors[] = 'Invalid Region';
-        }
-
-        // check password is strong enough
-        if (isset($params['password']) &&
-            !preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/', $params['password'])
-        ) {
-            $errors[] = 'Password not strong enough, must contain 1 lowercase letter,'
-                      . ' 1 uppercase letter, 1 number and be 8 characters or longer';
-        }
-
-        return $errors;
-    }
-
-    /**
-     * Builds the JSON for the request from the parameters provided
-     *
-     * @param string $requestId
-     * @param mixed[] $params
-     * @return string
-     */
-    private function buildRequest($requestId, $params)
-    {
-        $requestData = [
-            'Id' => $requestId,
-            'Platform' => $params['platform'],
-            'Email' => $params['email'],
-            'FirstName' => $params['firstname'],
-            'LastName' => $params['lastname'],
-            'Company' => $params['company'],
-            'Region' => Regions::getRegionName($params['region']),
-            'Currency' => $params['currency'],
-            'TimeZone' => $params['timezone'],
-            'Url' => $params['url'],
-            'Password' => $params['password'],
-            'StoreName' => $params['store_name']
+        $result = [
+            'status' => 0,
+            'response' => [],
+            'errors' => [],
+            'success' => false,
+            'request_id' => ''
         ];
 
-        return json_encode($requestData);
-    }
+        try {
+            $requestor = new Requestor();
+            $response = $requestor->send($this->getRequestId(), $params);
 
-    /**
-     * Validate URL and check that it has allowed scheme
-     *
-     * @param string $value
-     * @param array $allowedSchemes
-     * @return bool
-     */
-    public function isValidUrl($value, array $allowedSchemes = [])
-    {
-        $isValid = true;
-
-        if (!filter_var($value, FILTER_VALIDATE_URL)) {
-            $isValid = false;
+            $processor = new Processor();
+            $result = $processor->process($response);
+            $result['request_id'] = $this->getRequestId();
+        } catch (Exception $e) {
+            $result['errors'][] = 'Exception: ' . $e->getMessage();
         }
 
-        if ($isValid && !empty($allowedSchemes)) {
-            $url = parse_url($value);
-            if (empty($url['scheme']) || !in_array($url['scheme'], $allowedSchemes)) {
-                $isValid = false;
-            }
-        }
-        return $isValid;
+        return $result;
     }
 }
